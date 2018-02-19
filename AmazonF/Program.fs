4c03aaf018e92f99c5d1329f5d05878b74888701
oleksandr dubyna <oleksandr.dubyna@avanade.com>
2018-02-19 16:47:58 +0200

    u

--- AmazonF/Program.fs
+++ AmazonF/Program.fs
@@ -8,11 +8,26 @@ open FSharp.Data
 
 // Fetch the contents of a web page
 let fetchUrl callback url =        
-    let req = WebRequest.Create(Uri(url)) 
+    let req = WebRequest.Create(Uri(url)) :?> HttpWebRequest 
+    req.UserAgent <- "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
+
     use resp = req.GetResponse() 
     use stream = resp.GetResponseStream() 
     use reader = new IO.StreamReader(stream)
     callback reader url
+
+let fetchUrlStream url =        
+    let req = WebRequest.Create(Uri(url)) :?> HttpWebRequest 
+    req.UserAgent <- "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
+
+    use resp = req.GetResponse() 
+    use stream = resp.GetResponseStream() 
+    stream
+
+let GetStreamUrl pReauest pUrlBuilder pUrlPart =
+    let fullUrl:string = pUrlBuilder pUrlPart
+    pReauest fullUrl
+    
 
 let myCallback (reader:IO.StreamReader) url = 
     let html = reader.ReadToEnd()
@@ -20,27 +35,36 @@ let myCallback (reader:IO.StreamReader) url =
     printfn "Downloaded %s. First 1000 is %s" url html1000
     html      // return all the html
 
-let rec GetLinkNameUrl pUrlBuilder pUrl name listT = 
-    let fullUrl:string = pUrlBuilder pUrl
-    let doc = HtmlDocument.Load(fullUrl)
+let rec GetLinkNameUrl pGetStreamUrl pUrlPart name listT = 
+    let pUrlSteam = pGetStreamUrl pUrlPart
+    let doc = HtmlDocument.Parse(pUrlSteam)
+    let fds = (doc.Descendants "ul")
+    let fdsf = (doc.Descendants "ul").CssSelect(".s-ref-indent-two")
+    let dsa = doc.CssSelect("ul.a-unordered-list > .s-ref-indent-two") 
+    let dsa1 = doc.CssSelect("div.s-padding-right-mini s-border-right > ") 
     let res = doc.CssSelect("ul.s-ref-indent-two > div > li > span > a") 
                 |> Seq.choose (fun x -> x.TryGetAttribute "href"
                                              |> Option.map (fun a -> x.InnerText(), a.Value())
                               ) 
-    let hhhh = res |> Seq.map (fun c -> GetLinkNameUrl pUrlBuilder (fst c) (snd c) listT)
-    let hyh = (name, pUrl, res)::listT 
-    
+    //let hhhh = res |> Seq.map (fun c -> GetLinkNameUrl pGetStreamUrl (fst c) (snd c) listT)
+    //let hyh = (name, pUrl, res)::listT 
+    let fd= ""
+    let f1d= ""
     res
 let FullUrl l r = 
     sprintf "%s%s" l r
 [<EntryPoint>]
 let main argv = 
-    
+    let google = fetchUrl myCallback "https://www.amazon.com/s/ref=lp_283155_nr_n_0?fst=as%3Aoff&rh=n%3A283155%2Cn%3A%211000%2Cn%3A1&bbn=1000&ie=UTF8&qid=1518787887&rnid=1000"
     let baseUrl = "https://www.amazon.com"
-    let url =  GetLinkNameUrl (FullUrl baseUrl) "/s/ref=lp_283155_nr_n_0?fst=as%3Aoff&rh=n%3A283155%2Cn%3A%211000%2Cn%3A1&bbn=1000&ie=UTF8&qid=1518787887&rnid=1000" "123" List.empty
-    let gg =  url |> Seq.toList
-    let hh = fst gg.Head
-    let hh1 = snd gg.Head
+    let url =  GetLinkNameUrl 
+                (GetStreamUrl (fetchUrl myCallback) (FullUrl baseUrl))  
+                "/s/ref=lp_1_nr_n_0?fst=as%3Aoff&rh=n%3A283155%2Cn%3A%211000%2Cn%3A1%2Cn%3A173508&bbn=1&ie=UTF8&qid=1519043803&rnid=1"
+                "123" 
+                List.empty
+    //let gg =  url |> Seq.toList
+    //let hh = fst gg.Head
+   // let hh1 = snd gg.Head
     //let gg1 = GetLinkNameUrl 
 
     // Create & configure HTTP web request
